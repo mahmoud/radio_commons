7ac4d7cad0a330615d893ba307e0f916d9a8412b
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2013-10-05 20:11:10 -0700

    general playing around with temporary tables

---
+++ radio.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+from gzip import GzipFile
+from argparse import ArgumentParser
+
+
+def main(filename):
+    with GzipFile(filename) as gf:
+        data = gf.read(1024 * 1024 * 64)
+    import pdb;pdb.set_trace()
+
+
+def db_main(filename):
+    import sqlite3
+    conn = sqlite3.connect(':memory:')
+    cur = conn.cursor()
+    cur.execute('CREATE TABLE image (img_name, img_size, img_width, img_height, img_metadata, img_bits, img_media_type, img_major_mime, img_minor_mime, img_description, img_user, img_user_text, img_timestamp, img_sha1);')
+    
+    buff = u''
+    ii_start, ii_end = 0, None
+    with GzipFile(filename, 'rb') as gf:
+        data = gf.read(4096)
+        buff = data[data.index('INSERT INTO'):]
+        data = gf.read(1024 * 1024 * 2)
+        buff += data
+
+        ii_end = buff.find('INSERT INTO', 11)
+        if ii_end > 0:
+            full_statement, buff = buff[ii_start:ii_end], buff[ii_end:]
+            full_statement = full_statement.decode('utf-8')
+
+        replaced = full_statement.replace("\\'", "''")
+        splitted = replaced.split('),')
+        joined = u'),'.join(splitted[:200]) + ')'
+    full_statement = joined
+    cur.execute(full_statement)
+    conn.commit()
+    cur.execute('SELECT * FROM image')
+    res = cur.fetchall()
+    import pdb;pdb.set_trace()
+
+
+if __name__ == '__main__':
+    prs = ArgumentParser()
+    prs.add_argument('filename')
+    args = prs.parse_args()
+    db_main(args.filename)
